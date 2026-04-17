bd6644008a186497d2c114c0077c9190852762a0
cokw <kimjihoo9668@gmail.com>
2026-04-17 23:18:05 +0900

    ADD InstantDrawing.py
    ADD: 지우개 기능 추가 !

--- InstantDrawing.py
+++ InstantDrawing.py
@@ -7,7 +7,7 @@ BLACK = 0, 0, 0
 
 mainScreen = pygame.display.set_mode((720, 480))
 pygame.display.set_caption("Hello")
-drawing = False
+drawing = 0
 mainScreen.fill(WHITE)
 
 while True:
@@ -19,21 +19,25 @@ while True:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                drawing = True
+                drawing = 1
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 3:
+                 drawing = 2
 
         if event.type == pygame.MOUSEBUTTONUP:
-                drawing = False
+                drawing = 0
 
-        if drawing == True:
-
+        if drawing != 0:
             new_pos_x = int(event.pos[0] // 10) * 10
             new_pos_y = int(event.pos[1] // 10) * 10
             mouse_pos = (new_pos_x, new_pos_y)
 
+            if drawing == 1:
+                 pygame.draw.circle(mainScreen, BLACK, mouse_pos, 5)
 
-
-            pygame.draw.circle(mainScreen, BLACK, mouse_pos, 5)
-
+            if drawing == 2:
+                 pygame.draw.circle(mainScreen, WHITE, mouse_pos, 5)
 
     pygame.display.flip()
 
