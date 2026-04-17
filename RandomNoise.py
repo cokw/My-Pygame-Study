772ea4a0bd1c358b21355297f8cfc56574164844
cokw <kimjihoo9668@gmail.com>
2026-04-18 00:35:19 +0900

    Update RandomNoise.py
    FIX : 한번만 생성 후 정지되도록 수정

--- RandomNoise.py
+++ RandomNoise.py
@@ -12,15 +12,11 @@ i = 0; j = 0
 mainScreen = pygame.display.set_mode((720, 480))
 pygame.display.set_caption("RandomPixel")
 mainScreen.fill(WHITE)
+done = 0
 
-# 메인 루프
-while True:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
+def rand():
+    global done
+    done = 1
     for i in range(0, 720, 10):
         for j in range(0, 480, 10):
             dot_pos = (i, j)
@@ -29,6 +25,18 @@ while True:
                 pygame.draw.circle(mainScreen, BLACK, dot_pos, 5)
             else:
                 pygame.draw.circle(mainScreen, WHITE, dot_pos, 5)
+
+# 메인 루프
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    if done == 0:       
+        rand()
+    
+
 
 
 
